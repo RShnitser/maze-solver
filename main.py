ce6acc7a47ce3bfe135009be2a0800302c21249e
RShnitser <romanshnitser@hotmail.com>
2024-05-24 11:53:52 -0400

    add draw_line

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0, 50), Point(800, 50)), "black")
     win.wait_for_close()
 
 main()
